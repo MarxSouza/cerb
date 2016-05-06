9edff3208a867502d77d8c3aa10f8d7af2e60e93
Marx Souza <karlmarx12crvg@gmail.com>
2016-05-05 23:05:56 -0300

    to deploy

--- blog/models.py
+++ blog/models.py
@@ -37,4 +37,17 @@ class Noticia(models.Model):
 	
 	def __str__(self):
 		return "%s / %s" % (self.titulo, self.categoria)
-		
+	
+class Comentario(models.Model):
+	noticias = models.ForeignKey('blog.Noticia', related_name='comentarios')
+	autor = models.CharField(max_length=200)
+	texto = models.TextField()
+	data = models.DateTimeField(default=timezone.now)
+	verificacao = models.BooleanField(default=False)
+
+	def verificar(self):
+		self.aprovacao = True
+		self.save()
+
+	def __str__(self):
+		return self.texto
